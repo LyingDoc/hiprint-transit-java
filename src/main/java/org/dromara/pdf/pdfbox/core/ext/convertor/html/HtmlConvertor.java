4a532e17afe0e74c1546617c1a20a719ea6f99aa
Lyin <1187691002@qq.com>
2025-12-05 12:16:38 +0800

    版本：2.5.4   修复bug：   【converter】 还原html转化器自动注解生成

--- src/main/java/org/dromara/pdf/pdfbox/core/ext/convertor/html/HtmlConvertor.java
+++ src/main/java/org/dromara/pdf/pdfbox/core/ext/convertor/html/HtmlConvertor.java
@@ -23,12 +23,16 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Objects;
-import java.util.concurrent.*;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 import io.netty.util.concurrent.FastThreadLocal;
-import lombok.Generated;
+import lombok.EqualsAndHashCode;
+import lombok.Setter;
 import lombok.SneakyThrows;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
@@ -45,9 +49,13 @@ import org.dromara.pdf.pdfbox.util.IdUtil;
 import org.dromara.pdf.pdfbox.util.ImageUtil;
 import org.dromara.pdf.pdfbox.util.UnitUtil;
 
+@Setter
+@EqualsAndHashCode(callSuper = false)
 public class HtmlConvertor extends AbstractConvertor {
     protected static final FastThreadLocal<Page> THREAD_LOCAL;
     protected static final ThreadPoolExecutor POOL;
+    protected static final String UNIT = "px";
+
     protected Integer dpi;
     protected PageSize pageSize;
     protected Long requestTimeout;
@@ -208,11 +216,7 @@ public class HtmlConvertor extends AbstractConvertor {
 
     public String toHtml(String url, String domId) {
         this.init();
-        return StringUtils.toEncodedString(this.convert(url, (page) -> page.querySelector(domId).innerHTML().getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
-    }
-
-    protected BufferedImage convertToImage(String url) {
-        return ImageUtil.read(this.convertToImageBytes(url));
+        return StringUtils.toEncodedString(convert(url, (page) -> page.querySelector(domId).innerHTML().getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
     }
 
     @SneakyThrows
@@ -227,53 +231,8 @@ public class HtmlConvertor extends AbstractConvertor {
         } finally {
             Files.deleteIfExists(path);
         }
+
         return var3;
-    }
-
-    protected byte[] convertToPdfBytes(String url) {
-        Page.PdfOptions options = (new Page.PdfOptions())
-                .setWidth(UnitUtil.pt2px(this.dpi, this.pageSize.getWidth()) + "px")
-                .setHeight(UnitUtil.pt2px(this.dpi, this.pageSize.getHeight()) + "px")
-                .setPrintBackground(this.isIncludeBackground)
-                .setOutline(true)
-                .setScale((double) this.scale)
-                .setLandscape(this.isLandscape)
-                .setMargin((new Margin())
-                        .setLeft(this.marginLeft + "px")
-                        .setRight(this.marginRight + "px")
-                        .setTop(this.marginTop + "px")
-                        .setBottom(this.marginBottom + "px"));
-        return this.convert(url, (page) -> page.pdf(options));
-    }
-
-    protected Document imageToPdf(BufferedImage sourceImage) {
-        Document document = PdfHandler.getDocumentHandler().create();
-        document.setMarginTop(this.marginTop);
-        document.setMarginBottom(this.marginBottom);
-        document.setMarginLeft(this.marginLeft);
-        document.setMarginRight(this.marginRight);
-        org.dromara.pdf.pdfbox.core.base.Page page = new org.dromara.pdf.pdfbox.core.base.Page(document);
-        float scale = page.getWithoutMarginWidth() / (float)sourceImage.getWidth();
-
-        for(BufferedImage image : ImageUtil.splitForVertical(sourceImage, (int)(page.getWithoutMarginHeight() / scale))) {
-            Image component = new Image(document.getCurrentPage());
-            component.setImage(ImageUtil.scale(image, image.getWidth() * 2, image.getHeight() * 2, 1));
-            component.setScale(scale * this.scale / 2.0F);
-            component.setHorizontalAlignment(HorizontalAlignment.CENTER);
-            component.setVerticalAlignment(VerticalAlignment.CENTER);
-            component.render();
-        }
-
-        document.appendPage(page);
-        return document;
-    }
-
-    protected Page getBrowserPage() {
-        Page page = THREAD_LOCAL.get();
-        if (Objects.isNull(page)) {
-            page = this.initBrowserPage();
-        }
-        return page;
     }
 
     protected byte[] convertToImageBytes(String url) {
@@ -281,6 +240,26 @@ public class HtmlConvertor extends AbstractConvertor {
                 .setType(ScreenshotType.PNG)
                 .setFullPage(true)
                 .setOmitBackground(false)));
+    }
+
+    protected byte[] convertToPdfBytes(String url) {
+        Page.PdfOptions options = (new Page.PdfOptions())
+                .setWidth(UnitUtil.pt2px(this.dpi, this.pageSize.getWidth()) + UNIT)
+                .setHeight(UnitUtil.pt2px(this.dpi, this.pageSize.getHeight()) + UNIT)
+                .setPrintBackground(this.isIncludeBackground)
+                .setOutline(true).
+                setScale((double) this.scale)
+                .setLandscape(this.isLandscape)
+                .setMargin((new Margin())
+                        .setLeft(this.marginLeft + UNIT)
+                        .setRight(this.marginRight + UNIT)
+                        .setTop(this.marginTop + UNIT)
+                        .setBottom(this.marginBottom + UNIT));
+        return this.convert(url, (page) -> page.pdf(options));
+    }
+
+    protected BufferedImage convertToImage(String url) {
+        return ImageUtil.read(this.convertToImageBytes(url));
     }
 
     @SneakyThrows
@@ -312,10 +291,44 @@ public class HtmlConvertor extends AbstractConvertor {
                 end = System.currentTimeMillis();
                 this.log.info("Converted page: " + (end - begin) + " ms");
             }
+
             return bytes;
         }).get(5L, TimeUnit.MINUTES);
     }
 
+    protected Document imageToPdf(BufferedImage sourceImage) {
+        Document document = PdfHandler.getDocumentHandler().create();
+        document.setMarginTop(this.marginTop);
+        document.setMarginBottom(this.marginBottom);
+        document.setMarginLeft(this.marginLeft);
+        document.setMarginRight(this.marginRight);
+        org.dromara.pdf.pdfbox.core.base.Page page = new org.dromara.pdf.pdfbox.core.base.Page(document);
+        float scale = page.getWithoutMarginWidth() / (float) sourceImage.getWidth();
+
+        for (BufferedImage image : ImageUtil.splitForVertical(sourceImage, (int) (page.getWithoutMarginHeight() / scale))) {
+            Image component = new Image(document.getCurrentPage());
+            component.setImage(ImageUtil.scale(image, image.getWidth() * 2, image.getHeight() * 2, 1));
+            component.setScale(scale * this.scale / 2.0F);
+            component.setHorizontalAlignment(HorizontalAlignment.CENTER);
+            component.setVerticalAlignment(VerticalAlignment.CENTER);
+            component.render();
+        }
+
+        document.appendPage(page);
+        return document;
+    }
+
+    protected Page getBrowserPage() {
+        Page page = THREAD_LOCAL.get();
+        if (Objects.nonNull(page)) {
+            return page;
+        } else {
+            page = this.initBrowserPage();
+            return page;
+        }
+    }
+
+    @SneakyThrows
     protected Page initBrowserPage() {
         long begin = 0L;
         long end;
@@ -351,217 +364,8 @@ public class HtmlConvertor extends AbstractConvertor {
             end = System.currentTimeMillis();
             this.log.info("Initialized page: " + (end - begin) + " ms");
         }
+
         return newPage;
-    }
-
-    @Generated
-    public void setDpi(Integer dpi) {
-        this.dpi = dpi;
-    }
-
-    @Generated
-    public void setPageSize(PageSize pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    @Generated
-    public void setRequestTimeout(Long requestTimeout) {
-        this.requestTimeout = requestTimeout;
-    }
-
-    @Generated
-    public void setPageState(PageLoadState pageState) {
-        this.pageState = pageState;
-    }
-
-    @Generated
-    public void setMarginTop(Float marginTop) {
-        this.marginTop = marginTop;
-    }
-
-    @Generated
-    public void setMarginBottom(Float marginBottom) {
-        this.marginBottom = marginBottom;
-    }
-
-    @Generated
-    public void setMarginLeft(Float marginLeft) {
-        this.marginLeft = marginLeft;
-    }
-
-    @Generated
-    public void setMarginRight(Float marginRight) {
-        this.marginRight = marginRight;
-    }
-
-    @Generated
-    public void setScale(Float scale) {
-        this.scale = scale;
-    }
-
-    @Generated
-    public void setIsLandscape(Boolean isLandscape) {
-        this.isLandscape = isLandscape;
-    }
-
-    @Generated
-    public void setIsIncludeBackground(Boolean isIncludeBackground) {
-        this.isIncludeBackground = isIncludeBackground;
-    }
-
-    @Generated
-    public boolean equals(Object o) {
-        if (o == this) {
-            return true;
-        } else if (!(o instanceof HtmlConvertor)) {
-            return false;
-        } else {
-            HtmlConvertor other = (HtmlConvertor)o;
-            if (!other.canEqual(this)) {
-                return false;
-            } else if (!super.equals(o)) {
-                return false;
-            } else {
-                Object this$dpi = this.dpi;
-                Object other$dpi = other.dpi;
-                if (this$dpi == null) {
-                    if (other$dpi != null) {
-                        return false;
-                    }
-                } else if (!this$dpi.equals(other$dpi)) {
-                    return false;
-                }
-
-                Object this$requestTimeout = this.requestTimeout;
-                Object other$requestTimeout = other.requestTimeout;
-                if (this$requestTimeout == null) {
-                    if (other$requestTimeout != null) {
-                        return false;
-                    }
-                } else if (!this$requestTimeout.equals(other$requestTimeout)) {
-                    return false;
-                }
-
-                Object this$marginTop = this.marginTop;
-                Object other$marginTop = other.marginTop;
-                if (this$marginTop == null) {
-                    if (other$marginTop != null) {
-                        return false;
-                    }
-                } else if (!this$marginTop.equals(other$marginTop)) {
-                    return false;
-                }
-
-                Object this$marginBottom = this.marginBottom;
-                Object other$marginBottom = other.marginBottom;
-                if (this$marginBottom == null) {
-                    if (other$marginBottom != null) {
-                        return false;
-                    }
-                } else if (!this$marginBottom.equals(other$marginBottom)) {
-                    return false;
-                }
-
-                Object this$marginLeft = this.marginLeft;
-                Object other$marginLeft = other.marginLeft;
-                if (this$marginLeft == null) {
-                    if (other$marginLeft != null) {
-                        return false;
-                    }
-                } else if (!this$marginLeft.equals(other$marginLeft)) {
-                    return false;
-                }
-
-                Object this$marginRight = this.marginRight;
-                Object other$marginRight = other.marginRight;
-                if (this$marginRight == null) {
-                    if (other$marginRight != null) {
-                        return false;
-                    }
-                } else if (!this$marginRight.equals(other$marginRight)) {
-                    return false;
-                }
-
-                Object this$scale = this.scale;
-                Object other$scale = other.scale;
-                if (this$scale == null) {
-                    if (other$scale != null) {
-                        return false;
-                    }
-                } else if (!this$scale.equals(other$scale)) {
-                    return false;
-                }
-
-                Object this$isLandscape = this.isLandscape;
-                Object other$isLandscape = other.isLandscape;
-                if (this$isLandscape == null) {
-                    if (other$isLandscape != null) {
-                        return false;
-                    }
-                } else if (!this$isLandscape.equals(other$isLandscape)) {
-                    return false;
-                }
-
-                Object this$isIncludeBackground = this.isIncludeBackground;
-                Object other$isIncludeBackground = other.isIncludeBackground;
-                if (this$isIncludeBackground == null) {
-                    if (other$isIncludeBackground != null) {
-                        return false;
-                    }
-                } else if (!this$isIncludeBackground.equals(other$isIncludeBackground)) {
-                    return false;
-                }
-
-                Object this$pageSize = this.pageSize;
-                Object other$pageSize = other.pageSize;
-                if (this$pageSize == null) {
-                    if (other$pageSize != null) {
-                        return false;
-                    }
-                } else if (!this$pageSize.equals(other$pageSize)) {
-                    return false;
-                }
-
-                Object this$pageState = this.pageState;
-                Object other$pageState = other.pageState;
-                if (this$pageState == null) {
-                    return other$pageState == null;
-                } else return this$pageState.equals(other$pageState);
-            }
-        }
-    }
-
-    @Generated
-    protected boolean canEqual(Object other) {
-        return other instanceof HtmlConvertor;
-    }
-
-    @Generated
-    public int hashCode() {
-        int result = super.hashCode();
-        Object $dpi = this.dpi;
-        result = result * 59 + ($dpi == null ? 43 : $dpi.hashCode());
-        Object $requestTimeout = this.requestTimeout;
-        result = result * 59 + ($requestTimeout == null ? 43 : $requestTimeout.hashCode());
-        Object $marginTop = this.marginTop;
-        result = result * 59 + ($marginTop == null ? 43 : $marginTop.hashCode());
-        Object $marginBottom = this.marginBottom;
-        result = result * 59 + ($marginBottom == null ? 43 : $marginBottom.hashCode());
-        Object $marginLeft = this.marginLeft;
-        result = result * 59 + ($marginLeft == null ? 43 : $marginLeft.hashCode());
-        Object $marginRight = this.marginRight;
-        result = result * 59 + ($marginRight == null ? 43 : $marginRight.hashCode());
-        Object $scale = this.scale;
-        result = result * 59 + ($scale == null ? 43 : $scale.hashCode());
-        Object $isLandscape = this.isLandscape;
-        result = result * 59 + ($isLandscape == null ? 43 : $isLandscape.hashCode());
-        Object $isIncludeBackground = this.isIncludeBackground;
-        result = result * 59 + ($isIncludeBackground == null ? 43 : $isIncludeBackground.hashCode());
-        Object $pageSize = this.pageSize;
-        result = result * 59 + ($pageSize == null ? 43 : $pageSize.hashCode());
-        Object $pageState = this.pageState;
-        result = result * 59 + ($pageState == null ? 43 : $pageState.hashCode());
-        return result;
     }
 
     protected String getNavigateUrl(String url) {
